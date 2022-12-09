36db29f29b360905f6ed7a971f091f8668702e9d
annagorbateno <ahorbatenko@kse.org.ua>
2022-12-09 11:06:50 +0200

    initial code

---
+++ main.py
@@ -0,0 +1,11 @@
+def task1(filename,country,year):
+    head = None
+    is_first_line = True
+    with open(filename, "r") as file:
+        for line in file.readlines():
+            if is_first_line:
+                head = line.split("\t")
+                is_first_line = False
+                continue
+
+            head.index("NOC")
