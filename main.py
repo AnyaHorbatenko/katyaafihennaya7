2c393615f029e4e0afa5af7e4f68820c9670bf10
annagorbatenko <ahorbatenko@kse.org.ua>
2022-12-13 16:55:59 +0200

    add argparse

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def task1(filename,country,year):
+import argparse
+
+def task1(filenamee,countryy,yearr):
     head = None
     is_first_line = True
-    with open(filename, "r") as file:
+    with open(filenamee, "r") as file:
         for line in file.readlines():
             if is_first_line:
                 head = line.split("\t")
@@ -9,3 +11,18 @@ def task1(filename,country,year):
                 continue
 
             head.index("NOC")
+
+parser = argparse. ArgumentParser(description="")
+parser.add_argument("filename")
+parser.add_argument("--medals", action="store_true", required=False)
+parser.add_argument("--country", required=False)
+parser.add_argument("--year", required=False)
+args = parser.parse_args()
+countryy = args.country
+yearr = args.year
+filenamee = args.filename
+
+print(countryy, yearr)
+print(f"{args.filename=}")
+print(args)
+if medals == True:
