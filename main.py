e327039c57ad3ca80d1970dbc2d3d53856ec0b9e
annagorbatenko <ahorbatenko@kse.org.ua>
2022-12-15 10:50:24 +0200

    first task output bla bla

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 
+list_for_strochka = []
 def task1(filenamee,countryy,yearr):
     head = None
     people_count = 0
@@ -9,6 +10,8 @@ def task1(filenamee,countryy,yearr):
         # if is_first_line:
         linee = file.readline()
         if linee != "":
+            resultfile = open(args.output, "w")
+
             while people_count < 10:
                 line = linee.split("\t")
                 country = line[7]
@@ -20,11 +23,19 @@ def task1(filenamee,countryy,yearr):
                     if medals != "NA\n":
                         people_count += 1
                         print(f"{name} - {sport} - {medals}")
+                        list_for_strochka.append((f"{name} - {sport} - {medals}"))
+                        if args.output != None:
+                            resultfile.write(f"{name} - {sport} - {medals}")
+
                 linee = file.readline()
         # is_first_line = False
 
 def output_one():
-    pass
+    with open(newfile, "w") as file:
+        for line in list_for_strochka:
+            file.write(line)
+
+
 
 
 parser = argparse. ArgumentParser(description="")
@@ -32,15 +43,20 @@ parser.add_argument("filename")
 parser.add_argument("--medals", action="store_true", required=False)
 parser.add_argument("--country", required=False)
 parser.add_argument("--year", required=False)
+parser.add_argument("--output", required=False)
+
 args = parser.parse_args()
 medals = args.medals
 countryy = args.country
 yearr = args.year
 filenamee = args.filename
+newfile = args.output
 
 print(countryy, yearr)
 print(f"{args.filename=}")
 print(args)
 if medals == True:
     task1(filenamee, countryy, yearr)
+
+
 
